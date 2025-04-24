63b7afedcdde87d1c5384e3028a2f1fc8f8e7717
bruce.lu <bruce.lu@bridgewell.com>
2025-04-24 14:38:54 +0000

    feat: 更新 Board 方法以包含 lists 和 cards 的詳細資訊

--- graph/board.resolvers.go
+++ graph/board.resolvers.go
@@ -87,10 +87,36 @@ func (r *queryResolver) Board(ctx context.Context, id string) (*model.Board, err
 	if err != nil {
 		return nil, err
 	}
+	// 將 lists 及 cards 一併轉換
+	lists := make([]*model.List, 0, len(b.Lists))
+	for _, l := range b.Lists {
+		cards := make([]*model.Card, 0, len(l.Cards))
+		for _, c := range l.Cards {
+			cards = append(cards, &model.Card{
+				ID:        strconv.FormatUint(uint64(c.ID), 10),
+				Title:     c.Title,
+				Content:   strToPtr(c.Content),
+				ListID:    strconv.FormatUint(uint64(c.ListID), 10),
+				CreatedAt: c.CreatedAt.Format(utils.TimeFormat),
+				UpdatedAt: c.UpdatedAt.Format(utils.TimeFormat),
+				Position:  int32(c.Position),
+			})
+		}
+		lists = append(lists, &model.List{
+			ID:        strconv.FormatUint(uint64(l.ID), 10),
+			Name:      l.Name,
+			BoardID:   strconv.FormatUint(uint64(l.BoardID), 10),
+			CreatedAt: l.CreatedAt.Format(utils.TimeFormat),
+			UpdatedAt: l.UpdatedAt.Format(utils.TimeFormat),
+			Position:  int32(l.Position),
+			Cards:     cards,
+		})
+	}
 	return &model.Board{
 		ID:        strconv.FormatUint(uint64(b.ID), 10),
 		Name:      b.Name,
 		CreatedAt: b.CreatedAt.Format(utils.TimeFormat),
 		UpdatedAt: b.UpdatedAt.Format(utils.TimeFormat),
+		Lists:     lists,
 	}, nil
 }
